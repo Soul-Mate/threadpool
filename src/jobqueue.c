9e9979191aa34bf40a1686b8e853e9f9ab281fe8
yuanchang <yuanchang.xu@outlook.com>
2018-01-05 18:20:47 +0800

    thread pool

--- src/jobqueue.c
+++ src/jobqueue.c
@@ -1,70 +1,123 @@
 #include "threadpool.h"
 #include <stdio.h>
-int job_flag_init(job_flag * ptr_flag)
+
+/**
+ * @param struct job_flag *
+ * @return int
+ */
+int job_flag_init(job_flag **ptr_flag)
 {
-	ptr_flag = (job_flag *)malloc(sizeof(job_flag));
-	if (ptr_flag == NULL) {
+	(*ptr_flag) = (job_flag *)malloc(sizeof(job_flag));
+	if ((*ptr_flag) == NULL) {
 		return -1;
 	}
-	ptr_flag->flag = 0;
-	pthread_mutex_init(&ptr_flag->sign_mutex,NULL);
-	pthread_cond_init(&ptr_flag->sign_cond,NULL);
+	(*ptr_flag)->flag = 0;
+	pthread_mutex_init(&(*ptr_flag)->sign_mutex, NULL);
+	pthread_cond_init(&(*ptr_flag)->sign_cond, NULL);
 	return 0;
 }
 
+/**
+ * @param struct job_flag *
+ * @return int
+ */
+void job_flag_wait(job_flag *ptr_flag)
+{
+	pthread_mutex_lock(&ptr_flag->sign_mutex);
+	while (ptr_flag->flag == 0) {
+		pthread_cond_wait(&ptr_flag->sign_cond, &ptr_flag->sign_mutex);
+	}
+	ptr_flag->flag = 0;
+	pthread_mutex_unlock(&ptr_flag->sign_mutex);
+}
 
+
+/**
+ * @param struct job_flag *
+ */
+void job_flag_tell(job_flag *ptr_flag)
+{
+	pthread_mutex_lock(&ptr_flag->sign_mutex);
+	ptr_flag->flag = 1;
+	pthread_cond_signal(&ptr_flag->sign_cond);
+	pthread_mutex_unlock(&ptr_flag->sign_mutex);
+}
+
+
+
+/**
+ * @param struct job_queue **
+ * @return int
+ */
 int job_queue_init(job_queue **ptr_queue)
 {
-
 	(*ptr_queue) = (job_queue *)malloc(sizeof(job_queue));
-	if ((*ptr_queue) == NULL){
+	if ((*ptr_queue) == NULL) {
 		return -1;
 	}
 	job_flag * ptr_flag;
+
 	(*ptr_queue)->len = 0;
 	(*ptr_queue)->front = NULL;
 	(*ptr_queue)->rear = NULL;
-	if (job_flag_init(ptr_flag) < 0) {
+
+	if (job_flag_init(&ptr_flag) < 0) {
 		return -1;
 	}
+
 	(*ptr_queue)->has_job = ptr_flag;
-	pthread_rwlock_init(&(*ptr_queue)->queue_rwlock,NULL);
+
+	pthread_rwlock_init(&(*ptr_queue)->queue_rwlock, NULL);
 	return 0;
 }
 
+/**
+ * @param struct job_queue *
+ * @param struct job *
+ * @return void
+ */
 void job_queue_push(job_queue *ptr_queue, job *ptr_job)
 {
-
-	switch(ptr_queue->len) {
-		case 0:
-			ptr_queue->front = ptr_job;
-			ptr_queue->rear = ptr_job;
-			break;
-		default:
-			ptr_queue->rear->next = ptr_job;
-            ptr_queue->rear = ptr_job;
+	pthread_rwlock_wrlock(&ptr_queue->queue_rwlock);
+	switch (ptr_queue->len) {
+	case 0:
+		ptr_queue->front = ptr_job;
+		ptr_queue->rear = ptr_job;
+		break;
+	default:
+		ptr_queue->rear->next = ptr_job;
+		ptr_queue->rear = ptr_job;
 	}
 	ptr_queue->len++;
+	pthread_rwlock_unlock(&ptr_queue->queue_rwlock);
+	job_flag_tell(ptr_queue->has_job);
 }
 
 
+/**
+ * @param struct job_queue *
+ * @return struct job *
+ */
 job *job_queue_pull(job_queue *ptr_queue)
 {
+	pthread_rwlock_wrlock(&ptr_queue->queue_rwlock);
 	job *out_ptr_job;
-	switch(ptr_queue->len) {
-		case 0:
-			out_ptr_job = NULL;
-			break;
-		case 1:
-			out_ptr_job = ptr_queue->front;
-			ptr_queue->front = NULL;
-			ptr_queue->rear = NULL;
-			ptr_queue->len = 0;
-			break;
-		default:
-			out_ptr_job = ptr_queue->front;
-			ptr_queue->front = out_ptr_job->next;
-			ptr_queue->len--;
+	switch (ptr_queue->len) {
+	case 0:
+		out_ptr_job = NULL;
+		break;
+	case 1:
+		out_ptr_job = ptr_queue->front;
+		ptr_queue->front = NULL;
+		ptr_queue->rear = NULL;
+		ptr_queue->len = 0;
+		break;
+	default:
+		out_ptr_job = ptr_queue->front;
+		ptr_queue->front = out_ptr_job->next;
+		ptr_queue->len--;
 	}
+	pthread_rwlock_unlock(&ptr_queue->queue_rwlock);
+	job_flag_tell(ptr_queue->has_job);
 	return out_ptr_job;
 }
--- src/threadpool.c
+++ src/threadpool.c
@@ -2,48 +2,94 @@
 #include <stdlib.h>
 #include "threadpool.h"
 
-static int keep_alive = 0;
+static volatile int keep_alive = 0;
 
-int thpool_init(int nums)
+thread_pool * thpool_init(int nums)
 {
+	keep_alive = 1;
 	int i;
 	if (nums <= 0) {
-		return -1;
+		return NULL;
 	}
-	/* init thread pool */
-	th_pool * pool = (th_pool*)malloc(sizeof(th_pool));
+
+	thread_pool * pool = (thread_pool*)malloc(sizeof(thread_pool));
 	if (pool == NULL) {
-		return -1;
+		return NULL;
 	}
 	pool->work_num = 0;
 	pool->alive_num = 0;
-	pthread_mutex_init(&pool->pool_mutex,NULL);
-	pthread_rwlock_init(&pool->pool_rwlock,NULL);
-	pthread_cond_init(&pool->pool_cond,NULL);
-	pool->threads = (th**)malloc(sizeof(th*) * nums);
-	
-	/*create thread*/
-	for (i = 0; i < nums; ++i) {
-		thread_create(pool->threads[i],i,pool);
+	pthread_mutex_init(&pool->pool_mutex, NULL);
+	pthread_rwlock_init(&pool->pool_rwlock, NULL);
+	pthread_cond_init(&pool->pool_cond, NULL);
+	pool->threads = (thread**)malloc(sizeof(thread*) * nums);
+
+	if (job_queue_init(&pool->thread_job_queue) != 0) {
+		free(pool);
 	}
-	while(1); // TODO test thread run
-	return 0;
+	for (i = 0; i < nums; ++i) {
+		thread_create(pool->threads[i], i, pool);
+	}
+	while (pool->alive_num != nums);
+	return pool;
 }
 
-int thread_create(th *thread, int i, th_pool *pool)
+void thpool_wait(thread_pool *ptr_pool)
 {
-	thread = (th *)malloc(sizeof(th));
-	thread->id = i;
-	pthread_create(&(thread->tid),NULL,thread_run,(void *)pool);
+	pthread_cond_wait(&ptr_pool->pool_cond,&ptr_pool->pool_mutex);
+}
+
+int thpool_add_work(thread_pool *pool, void *(*func)(void *arg), void *arg)
+{
+	job *ptr_job = (job *)malloc(sizeof(job));
+	if (ptr_job == NULL) {
+		return -1;
+	}
+	ptr_job->func = func;
+	ptr_job->arg = arg;
+	job_queue_push(pool->thread_job_queue, ptr_job);
+}
+
+int thread_create(thread *ptr_thread, int i, thread_pool *pool)
+{
+
+	ptr_thread = (thread *)malloc(sizeof(thread));
+	ptr_thread->id = i;
+	pthread_create(&(ptr_thread->tid), NULL, thread_run, (void *)pool);
 	return 0;
 }
 
 void *thread_run(void *arg)
 {
-	th_pool *pool = (th_pool *)arg;
-	pthread_mutex_lock(&pool->pool_mutex);
-	printf("%s\n", "hello world");
-	pthread_mutex_unlock(&pool->pool_mutex);
+	thread_pool *pool = (thread_pool *)arg;
+	pthread_rwlock_wrlock(&pool->pool_rwlock);
+	pool->alive_num++;
+	pthread_rwlock_unlock(&pool->pool_rwlock);
+
+	for (;;) {
+		job_flag_wait(pool->thread_job_queue->has_job);
+
+		if (keep_alive) {
+			pthread_rwlock_wrlock(&pool->pool_rwlock);
+			pool->work_num++;
+			pthread_rwlock_unlock(&pool->pool_rwlock);
+
+			job *ptr_job = job_queue_pull(pool->thread_job_queue);
+			if (ptr_job) {
+				ptr_job->func(ptr_job->arg);
+				free(ptr_job);
+			}
+			
+			pthread_rwlock_wrlock(&pool->pool_rwlock);
+			pool->work_num--;
+			if (!pool->work_num) {
+				pthread_cond_signal(&pool->pool_cond);
+			}
+			pthread_rwlock_unlock(&pool->pool_rwlock);
+		}
+	}
+	pthread_rwlock_wrlock(&pool->pool_rwlock);
+	pool->alive_num--;
+	pthread_rwlock_unlock(&pool->pool_rwlock);
 }
 
 
